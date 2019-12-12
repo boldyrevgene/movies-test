b11538e7856e049c372ce4cc480743247526391a
Eugene Boldyrev <boldyrev.gene@gmail.com>
2019-12-12 15:31:10 +0200

    movies service favorites functionality

--- src/app/movie.service.ts
+++ src/app/movie.service.ts
@@ -1,9 +1,9 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, of } from 'rxjs';
 
 
-import { Movie, SearchResult } from './types';
+import { Movie, MoviesDictionary, SearchResult } from './types';
 
 
 @Injectable({
@@ -11,30 +11,65 @@ import { Movie, SearchResult } from './types';
 })
 export class MovieService {
 
-  constructor(private http: HttpClient, private apiKey: string) { }
+  private static readonly STORE_KEY = 'favorite_movies';
+
+  private readonly favorites: MoviesDictionary;
+
+  constructor(private http: HttpClient, private apiKey: string) {
+    this.favorites = this.loadFavorites();
+  }
 
   search(query: string, page: number = 1): Observable<SearchResult> {
 
     const params = new HttpParams({
       fromObject: {
-        apikey: this.apiKey,
         s: query,
         page: '' + page
       }
     });
 
-    return this.http.get('http://www.omdbapi.com', {params}) as Observable<SearchResult>;
+    return this.makeRequest(params) as Observable<SearchResult>;
   }
 
   getMovie(id: string): Observable<Movie> {
-    return null;
+    if (this.favorites.hasOwnProperty(id)) {
+      return of(this.favorites[id]);
+    }
+
+    const params = (new HttpParams()).append('i', id);
+    return this.makeRequest(params);
   }
 
   isFavorite(movie: Movie): boolean {
-    return false;
+    return this.favorites.hasOwnProperty(movie.imdbID);
   }
 
   setAsFavorite(movie: Movie, isFavorite: boolean): void {
+    if (!isFavorite && this.favorites.hasOwnProperty(movie.imdbID)) {
+      delete this.favorites[movie.imdbID];
+    }
 
+    if (isFavorite) {
+      this.favorites[movie.imdbID] = movie;
+    }
+
+    this.storeFavorites(this.favorites);
+  }
+
+  private makeRequest(params: HttpParams): Observable<any> {
+    return this.http.get(
+      'http://www.omdbapi.com',
+      {params: params.append('apikey', this.apiKey)}
+    );
+  }
+
+  private loadFavorites(): MoviesDictionary {
+    const storedData = localStorage.getItem(MovieService.STORE_KEY);
+
+    return storedData ? JSON.parse(storedData) : {};
+  }
+
+  private storeFavorites(favorites: MoviesDictionary): void {
+    localStorage.setItem(MovieService.STORE_KEY, JSON.stringify(favorites));
   }
 }
